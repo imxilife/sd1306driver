9d434e4df7cbc431634c6c2a7b88376299b0074f
kelly <imaxthon@163.com>
2022-12-14 23:36:19 +0800

    完成画线功能

--- src/main.cpp
+++ src/main.cpp
@@ -16,6 +16,7 @@
  */
 
 #include <Arduino.h>
+#include <math.h>
 #include "./iic/iic.h"
 
 int led = 2;
@@ -160,46 +161,6 @@ void fullScreen()
   }
 }
 
-/**
- *在任意坐标绘制一个点
- */
-void drawPointForXY()
-{
-
-  /*   if(x>127||y>63){
-      Serial1.println("坐标点不合法...");
-      return;
-    } */
-
-  writeCmd(0xb0); // 页指针指向0
-  for (int i = 0; i < 16; i++)
-  {
-    writeCmd(0x0 + i); // 列地位地址
-    writeCmd(0x10);    // 列高位地址
-    writeData(0xff);   // 写入数据1
-  }
-
-  for (int i = 0; i < 16; i++)
-  {
-    writeCmd(0x0 + i); // 列低位地址
-    writeCmd(0x11);    // 列高位地址
-    writeData(0xff);   // 写入数据1
-  }
-
-  for (int i = 0; i < 16; i++)
-  {
-    writeCmd(0x0 + i); // 列低位地址
-    writeCmd(0x12);    // 列高位地址
-    writeData(0xff);   // 写入数据1
-  }
-
-  for (int i = 0; i < 16; i++)
-  {
-    writeCmd(0x0 + i); // 列低位地址
-    writeCmd(0x17);    // 列高位地址
-    writeData(0xff);   // 写入数据1
-  }
-}
 
 /**
  * @param x rang in [0~127]
@@ -235,7 +196,153 @@ void drawPointWithXY(int x, int y, int val)
   writeCmd(lowAddr);
   writeCmd(hightAddr);
 
-  writeData(pointVal);
+  writeData(pointVal<<pointY);
+}
+
+
+/**
+ *  两点式求直线方程
+ *  (y-y1)/(y2-y1) = (x-x1)/(x2-x1)
+ *   if(x1>x2) => x∈[x1,x2]
+ *   else  x∈[x2,x1]
+ *
+ */
+void drawLine(int x1, int y1, int x2, int y2)
+{
+
+  // 根据两点式求解直线方程
+
+  // 确定x的方向
+  int xDirection = x2 - x1 > 0 ? 1 : 0;
+  int array[xDirection?x2+1:x1+1];
+  // 确定x的取值范围
+
+  int arrayLength = sizeof(array)/4;
+
+  if(xDirection){
+    array[0] = x1;
+    array[sizeof(array)-1] = x2;
+  }else{
+    array[0] = x2;
+    array[sizeof(array)-1] = x1;
+  }
+  //Serial.println(arrayLength);
+   for (size_t i = 1; i <= arrayLength -1; i++)
+  {
+    array[i] = xDirection ? x1 + i : x2 + i;
+  } 
+
+  
+  //int xy[size][size];
+  
+  Serial.println("start print...");
+
+  int x, y;
+  int x2x1 = (x2 - x1);
+  int y2y1 = (y2 - y1);
+
+  for (size_t j = 0; j < arrayLength; j++)
+  {
+
+    if(j==0){
+      Serial.print("x=");
+      Serial.print(x1);
+      Serial.print(" | ");
+      Serial.print("y=");
+      Serial.print(y1);
+      Serial.println(",");
+    }else if (j==arrayLength)
+    {
+      Serial.print("x=");
+      Serial.print(x2);
+      Serial.print(" | ");
+      Serial.print("y=");
+      Serial.print(y2);
+      Serial.println("1,");
+    }else{
+
+      y = (array[j]-x1)*y2y1/x2x1+y1;
+      Serial.print("x=");
+      Serial.print(array[j]);
+      Serial.print(" | ");
+      Serial.print("y=");
+      Serial.print(y);
+      Serial.println(",");
+    }
+    
+
+     if(j==0){
+      drawPointWithXY(x1,y1,0x01);
+    }else if (j==arrayLength-1)
+    {
+      drawPointWithXY(x2,y2,0x01);
+    }else{
+       y = (array[j] - x1) * y2y1 / x2x1 + y1;
+      drawPointWithXY(array[j],y,0x01);
+      Serial.println(y);
+    } 
+  }
+}
+
+/**
+ * 使用第二种方法画线
+*/
+void drawLineBySecond(int x1,int y1,int x2,int y2){
+
+  float x,y;
+
+  float xx1,yy1,xx2,yy2;
+  xx1 = x1;
+  yy1 = y1;
+  xx2 = x2;
+  yy2 = y2;
+
+  if(x1==x2){
+    Serial.println("x1≠x2");
+    return;
+  }
+
+  //计算斜率
+  float k = (yy2-yy1)/(xx2-xx1);
+
+  Serial.print("斜率k:");
+  Serial.println(k);
+
+  int direction = x2-x1>0?1:0;
+  float array[abs(x2-x1)+1];
+  int arrayLen = sizeof(array)/4;
+  Serial.println(arrayLen);
+
+  for (size_t i = 0; i < arrayLen; i++)
+  {
+      if(i==0||i==(arrayLen-1)){
+         if(i==0){
+            array[0] = direction?xx1:xx2;
+            drawPointWithXY(array[0],y1,0x01);
+         }else{
+            array[arrayLen-1] = direction?xx2:xx1;
+            drawPointWithXY(array[arrayLen-1],y2,0x01);
+         }
+      }else
+      {
+        array[i] = direction?xx1+i:xx2+i;
+        y = round(k*array[i]);
+        drawPointWithXY(array[i],y,0x01);
+      }
+  }
+
+  for (size_t i = 0; i < arrayLen; i++)
+  {
+      y = round(k*array[i]);
+       Serial.print("x=");
+       Serial.print(array[i]);
+       Serial.print(" | ");
+       Serial.print("y=");
+       Serial.print(y);
+       Serial.println(",");
+
+  }
+  
 }
 
 void clearFirstRow()
@@ -257,27 +364,20 @@ void setup()
   initSSD1306();
   clear();
   clearFirstRow();
-  // delay(1);
-  // fullScreen();
-
-  // delay(10);
-
-  // clear();
-
-  // clear();
-
-  // drawPointForXY();
 }
 
 void loop()
 {
 
-  //drawPointForXY();
+  // drawPointForXY();
 
-  drawPointWithXY(0,0,1);
-  drawPointWithXY(127,0,1);
-  drawPointWithXY(0,63,1);
-  drawPointWithXY(127,63,1);
+  //drawPointWithXY(0, 0, 1);
+  //drawPointWithXY(127, 0, 1);
+  //drawPointWithXY(0, 63, 1);
+  //drawPointWithXY(127, 63, 1);
+
+  drawLine(0,0,127,63);
+  //drawLineBySecond(0,0,127,63);
 
   while (true)
   {
