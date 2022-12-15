5541226bf78bcc792dd0343f24b26fdca4cac72c
kelly <imaxthon@163.com>
2022-12-15 10:18:46 +0800

    创建图形库

---
+++ src/gui/canvas.cpp
@@ -0,0 +1,65 @@
+
+
+/**
+ * @brief 画点
+ * 
+ */
+void drawPoint(){
+
+}
+
+/**
+ * @brief 绘制线
+ * 
+ */
+void drawLine(){
+
+}
+
+/**
+ * @brief 绘制横线
+ * 
+ */
+void drawHLine(){
+
+}
+
+/**
+ * @brief 绘制竖线
+ * 
+ */
+void drawVLine(){
+
+}
+
+/**
+ * @brief 绘制圆
+ * 
+ */
+void drawCircle(){
+
+}
+
+/**
+ * @brief 绘制矩形
+ * 
+ */
+void drawRect(){
+
+}
+
+/**
+ * @brief 绘制圆角矩形
+ * 
+ */
+void drawRoundRect(){
+
+}
+
+/**
+ * @brief 绘制椭圆
+ * 
+ */
+void drawOval(){
+
+}
---
+++ src/gui/canvas.hpp
@@ -0,0 +1,21 @@
+
+
+#ifndef CANVAS_H_
+#define CANVAS_H_
+
+
+class Canvas{
+
+    public:
+    void drawPoint();
+    void drawLine();
+    void drawHLine();
+    void drawVLine();
+    void drawCircle();
+    void drawOval();
+    void drawRect();
+    void drawRoundRect();
+
+};
+
+#endif
